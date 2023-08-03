0565cdb7bcfcafe37a6703a73f8709d1a516df6d
kwlandry-acoustic <kip.landry@acoustic.com>
2023-08-03 15:47:38 -0400

    rebuild s3 bucket
    Signed-off-by: kwlandry-acoustic <kip.landry@acoustic.com>

--- src/handlers/s3-json-logger.ts
+++ src/handlers/s3-json-logger.ts
@@ -2,7 +2,7 @@ import { Metrics, MetricUnits } from '@aws-lambda-powertools/metrics';
 import { Logger } from '@aws-lambda-powertools/logger';
 import { Tracer } from '@aws-lambda-powertools/tracer';
 
-//run it again and again and 8
+//run it again and again and 9
 
 // Create a client to read objects from S3
 import { Context, S3Event } from 'aws-lambda';
