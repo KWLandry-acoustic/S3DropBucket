7207ce627e401d258112a6edc8d13d713c2ca522
kwlandry-acoustic <kip.landry@acoustic.com>
2023-08-16 09:54:57 -0400

    resolve aws v3
    Signed-off-by: kwlandry-acoustic <kip.landry@acoustic.com>

--- src/handlers/s3-json-logger.ts
+++ src/handlers/s3-json-logger.ts
@@ -1,16 +1,10 @@
-import { config, S3, Lambda, HttpRequest, HttpResponse, HTTPOptions, Endpoint } from 'aws-sdk'
-import { Handler, S3Event, Context } from 'aws-lambda';
-import s3 from 'aws-sdk/clients/s3';
-
 // // this imports a bare-bones version of S3 that exposes the .send operation
-// import { S3Client } from "@aws-sdk/client-s3"
+import { S3Client, S3ClientConfig } from "@aws-sdk/client-s3"
+import { Handler, S3Event, Context } from 'aws-lambda';
+
 
 // // this imports just the getObject operation from S3
-// import { GetObjectCommand } from "@aws-sdk/client-s3"
-
-// //usage
-// const bareBonesS3 = new S3Client({...});
-// await bareBonesS3.send(new GetObjectCommand({...}));
+import { GetObjectCommand } from "@aws-sdk/client-s3"
 
 
 
@@ -20,7 +14,7 @@ import s3 from 'aws-sdk/clients/s3';
   */
 
 // Create a client to read objects from S3
-const s3Client = new S3({ httpOptions: { timeout: 900 } });
+const s3Client = new S3Client({ httpOptions: { timeout: 900 } });
 
 
 export const s3JsonLoggerHandler: Handler = async (event: S3Event, context: Context) => {
@@ -28,6 +22,11 @@ export const s3JsonLoggerHandler: Handler = async (event: S3Event, context: Cont
     console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2))
     console.log('EVENT: \n' + JSON.stringify(event, null, 2));
     console.warn("What does 'Warn' look like ?")
+
+// // //usage
+// const s3Config: S3ClientConfig = "" 
+// const bareBonesS3 = new S3Client(s3Config.endpoint);
+// await bareBonesS3.send(new GetObjectCommand({...}));
 
 
     const params: S3.GetObjectRequest = {
@@ -142,48 +141,48 @@ export async function postCampaign(apiCall: string) {
 
 
 
-    // const getObjectRequest = event.Records.map(async (record) => {
-    //     // event.Records.map(async (record) => {
+// const getObjectRequest = event.Records.map(async (record) => {
+//     // event.Records.map(async (record) => {
 
-    //     const params = {
-    //         Bucket: record.s3.bucket.name,
-    //         Key: record.s3.object.key,
-    //     }
+//     const params = {
+//         Bucket: record.s3.bucket.name,
+//         Key: record.s3.object.key,
+//     }
 
-    //     const r = await s3.getObject(params).promise()
-    //         .catch(function (err) {
-    //             console.log("Promise Exception-Message: \n", err.message);
-    //             console.log("Promise Exception-Stack: \n", err.stack);
-    //         })
-    //         .then(function (d) {
-    //             console.log("await getObject:\n", d)
-    //         })
+//     const r = await s3.getObject(params).promise()
+//         .catch(function (err) {
+//             console.log("Promise Exception-Message: \n", err.message);
+//             console.log("Promise Exception-Stack: \n", err.stack);
+//         })
+//         .then(function (d) {
+//             console.log("await getObject:\n", d)
+//         })
 
-    //     // console.log("This is line 47 r: \n\n", r)
+//     // console.log("This is line 47 r: \n\n", r)
 
-    //     return {
-    //         statusCode: 200,
-    //         body: JSON.stringify(r)
-    //     }
+//     return {
+//         statusCode: 200,
+//         body: JSON.stringify(r)
+//     }
 
-    // })
+// })
 
-    // await Promise.all(getObjectRequest)
-    //     .catch(function (err) {
-    //         console.log("await Promise.all GetObjectRequests Promise Exception-Message: \n", err.message);
-    //         console.log("await Promise.all GetObjectRequests Promise Exception-Stack: \n", err.stack);
-    //         // tracer.addErrorAsMetadata(err as Error);
-    //         logger.error(`Error response from API endpoint: ${err}`)
-    //     })
-    //     .then(function (res) {
-    //         console.log("This is await Promise.all GetObjectRequests - res: \n", res)
-    //         logger.info(`Successful response from API endpoint: ${event.Records}`, JSON.stringify(res));
+// await Promise.all(getObjectRequest)
+//     .catch(function (err) {
+//         console.log("await Promise.all GetObjectRequests Promise Exception-Message: \n", err.message);
+//         console.log("await Promise.all GetObjectRequests Promise Exception-Stack: \n", err.stack);
+//         // tracer.addErrorAsMetadata(err as Error);
+//         logger.error(`Error response from API endpoint: ${err}`)
+//     })
+//     .then(function (res) {
+//         console.log("This is await Promise.all GetObjectRequests - res: \n", res)
+//         logger.info(`Successful response from API endpoint: ${event.Records}`, JSON.stringify(res));
 
-    //         return {
-    //             statusCode: 200,
-    //             body: JSON.stringify(res)
-    //         }
-    //     })
+//         return {
+//             statusCode: 200,
+//             body: JSON.stringify(res)
+//         }
+//     })
 
 
 // }
@@ -193,108 +192,108 @@ export async function postCampaign(apiCall: string) {
 
 // try {
 
-    // const interval = 2000; // Blocks the main thread in 100 millisecond interval
-    // //const blockingInterval = setInterval(() => undefined, 100)
-    // const blockingInterval = setInterval(function () {
+// const interval = 2000; // Blocks the main thread in 100 millisecond interval
+// //const blockingInterval = setInterval(() => undefined, 100)
+// const blockingInterval = setInterval(function () {
 
-    //     // await getObjectS3("simple-app-bucket", "test/key")
-    //     //     .then(value => {
-    //     //         clearInterval(blockingInterval)
-    //     //         // ... Rest of code here
-    //     //     })
-    //     //     .catch(err => {
-    //     //         clearInterval(blockingInterval)
-    //     //         console.log("Exception in Wait: \n", err)
-    //     //     })
+//     // await getObjectS3("simple-app-bucket", "test/key")
+//     //     .then(value => {
+//     //         clearInterval(blockingInterval)
+//     //         // ... Rest of code here
+//     //     })
+//     //     .catch(err => {
+//     //         clearInterval(blockingInterval)
+//     //         console.log("Exception in Wait: \n", err)
+//     //     })
 
-    //     console.log("This should be fun: \n")
-    // }, interval)
+//     console.log("This should be fun: \n")
+// }, interval)
 
-    // const params = {
-    //     Bucket: "simple-app-bucket",
-    //     Key: "test/key",
-    // }
+// const params = {
+//     Bucket: "simple-app-bucket",
+//     Key: "test/key",
+// }
 
-    // async function getObjectS3(bucket, objectKey) {
-    //     try {
-    //         const params = {
-    //             Bucket: bucket,
-    //             Key: objectKey
-    //         }
+// async function getObjectS3(bucket, objectKey) {
+//     try {
+//         const params = {
+//             Bucket: bucket,
+//             Key: objectKey
+//         }
 
-    //         return s3.getObject(params).Body
-    //         //const data = await s3.getObject(params).promise()
-    //         // .catch(function (err) {
-    //         //     console.log("Promise Exception-Message: \n", err.message);
-    //         //     console.log("Promise Exception-Stack: \n", err.stack);
-    //         // })
-    //         // .then(function (d) {
-    //         //     console.log("This is getObjectS3: \n", d)
-    //         // })
+//         return s3.getObject(params).Body
+//         //const data = await s3.getObject(params).promise()
+//         // .catch(function (err) {
+//         //     console.log("Promise Exception-Message: \n", err.message);
+//         //     console.log("Promise Exception-Stack: \n", err.stack);
+//         // })
+//         // .then(function (d) {
+//         //     console.log("This is getObjectS3: \n", d)
+//         // })
 
-    //         return data.Body.toString('utf-8');
+//         return data.Body.toString('utf-8');
 
-    //     } catch (e) {
-    //         console.log("function GetObjectS3 Exception-Message: \n", e.message);
-    //         console.log("function GetObjectS3 Exception-Stack: \n", e.stack);
-    //         // console.log(`Could not retrieve file from S3\n: ${e.message}`)
-    //     }
-    // }
-
-
-
-
-    // try {
-
-    // await getObjectS3(params)
-    //     .catch(function (err) {
-    //         console.log("Await GetObjectS3 Promise Exception-Message: \n", err.message);
-    //         console.log("Await GetObjectS3 Promise Exception-Stack: \n", err.stack);
-    //     })
-    //     .then(function (res) {
-    //         console.log("This is Await GetObjectS3 - res: \n", res)
-    //     })
-    // //console.log("This is Resp: \n", s3r)
-
-
-    // await Promise.all(getObjectS3(params))
-    //     .catch(function (err) {
-    //         console.log("Await Promise.all GetObjectS3 Promise Exception-Message: \n", err.message);
-    //         console.log("Await Promise.all GetObjectS3 Promise Exception-Stack: \n", err.stack);
-    //     })
-    //     .then(function (res) {
-    //         console.log("This is Await Promise.all GetObjectS3 - res: \n", res)
-    //     })
-    // //console.log("This is Resp: \n", s3r)
-
-
-    // await Promise.all(getObjectRequest)
-    //     .catch(function (err) {
-    //         console.log("await Promise.all GetObjectRequests Promise Exception-Message: \n", err.message);
-    //         console.log("await Promise.all GetObjectRequests Promise Exception-Stack: \n", err.stack);
-    //     })
-    //     .then(function (res) {
-    //         console.log("This is await Promise.all GetObjectRequests - res: \n", res)
-    //     })
-    // //console.log("This is Resp: \n", gor)
-
-
-    // const s = await getObject(params)
-    // console.log("This is S: \n", s)
-
-    // const t = await getObjectRequests().catch(function (err) {
-    //     console.log("Await Promise Exception-Message: \n", err.message);
-    //     console.log("Await Promise Exception-Stack: \n", err.stack);
-    // })
+//     } catch (e) {
+//         console.log("function GetObjectS3 Exception-Message: \n", e.message);
+//         console.log("function GetObjectS3 Exception-Stack: \n", e.stack);
+//         // console.log(`Could not retrieve file from S3\n: ${e.message}`)
+//     }
+// }
 
 
 
-    //     // console.log("This is r: \n\n", r)
 
-    //     //const response = await getObjectRequests().then()
-    //     //const r = await response.Records
+// try {
 
-    //     //console.log("This is the response: \n\n", response)
+// await getObjectS3(params)
+//     .catch(function (err) {
+//         console.log("Await GetObjectS3 Promise Exception-Message: \n", err.message);
+//         console.log("Await GetObjectS3 Promise Exception-Stack: \n", err.stack);
+//     })
+//     .then(function (res) {
+//         console.log("This is Await GetObjectS3 - res: \n", res)
+//     })
+// //console.log("This is Resp: \n", s3r)
+
+
+// await Promise.all(getObjectS3(params))
+//     .catch(function (err) {
+//         console.log("Await Promise.all GetObjectS3 Promise Exception-Message: \n", err.message);
+//         console.log("Await Promise.all GetObjectS3 Promise Exception-Stack: \n", err.stack);
+//     })
+//     .then(function (res) {
+//         console.log("This is Await Promise.all GetObjectS3 - res: \n", res)
+//     })
+// //console.log("This is Resp: \n", s3r)
+
+
+// await Promise.all(getObjectRequest)
+//     .catch(function (err) {
+//         console.log("await Promise.all GetObjectRequests Promise Exception-Message: \n", err.message);
+//         console.log("await Promise.all GetObjectRequests Promise Exception-Stack: \n", err.stack);
+//     })
+//     .then(function (res) {
+//         console.log("This is await Promise.all GetObjectRequests - res: \n", res)
+//     })
+// //console.log("This is Resp: \n", gor)
+
+
+// const s = await getObject(params)
+// console.log("This is S: \n", s)
+
+// const t = await getObjectRequests().catch(function (err) {
+//     console.log("Await Promise Exception-Message: \n", err.message);
+//     console.log("Await Promise Exception-Stack: \n", err.stack);
+// })
+
+
+
+//     // console.log("This is r: \n\n", r)
+
+//     //const response = await getObjectRequests().then()
+//     //const r = await response.Records
+
+//     //console.log("This is the response: \n\n", response)
 
 
 // }
