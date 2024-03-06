28d2aa4b172bdd2c00498aebf865c64f8d764daa
kwlandry-acoustic <kip.landry@acoustic.com>
2024-03-06 15:53:43 -0500

    Remove Test Config
    Signed-off-by: kwlandry-acoustic <kip.landry@acoustic.com>

--- src/handlers/s3DropBucket.ts
+++ src/handlers/s3DropBucket.ts
@@ -51,11 +51,11 @@ import sftpClient, { ListFilterFunction } from 'ssh2-sftp-client'
 
 let testS3Key: string
 let testS3Bucket: string
-testS3Bucket = "tricklercache-configs"
+// testS3Bucket = "tricklercache-configs"
 // testS3Key = "TestData/pura_2024_02_26T05_53_26_084Z.json"
 // testS3Key = "TestData/visualcrossing_00213.csv"
 // testS3Key = "TestData/pura_2024_02_25T00_00_00_090Z.json"
-testS3Key = "TestData/pura_aggregate_S3DropBucket_Aggregator-7-2024-03-05-20-07-28-ae512353-e614-348c-86ac-43aa1236f117.json"
+// testS3Key = "TestData/pura_aggregate_S3DropBucket_Aggregator-7-2024-03-05-20-07-28-ae512353-e614-348c-86ac-43aa1236f117.json"
 
 
 
@@ -615,7 +615,18 @@ async function processS3ObjectContentStream (key: string, version: string, bucke
             })               //, { objectMode: true })
 
 
-
+            //At this point, all data, whether a json file or a csv file parsed by csvparser should come through
+            //  as a series of Objects (One at a time in the Stream) with a line break after each Object.
+            //   {data:data, data:data, .....}
+            //   {data:data, data:data, .....}
+            //   {data:data, data:data, .....}
+            //
+            // Later, OnData processing populates an Array with each line/Object, so the
+            // final format will be an Array of Objects:
+            // [ {data:data, data:data, .....}
+            //  { data: data, data: data, .....}
+            //  { data: data, data: data, .....} ]
+            //
 
             // s3ContentReadableStream = s3ContentReadableStream.pipe(t).pipe(jsonParser)
             s3ContentReadableStream = s3ContentReadableStream.pipe(jsonParser)
