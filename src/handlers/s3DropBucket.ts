a3aea577960253d5101357eadcf010166eb48e2c
kwlandry-acoustic <Kip.Landry@acoustic.co>
2024-11-07 00:59:41 -0500

    resolve selectivelogging
    Signed-off-by: kwlandry-acoustic <Kip.Landry@acoustic.co>

--- src/handlers/s3DropBucket.ts
+++ src/handlers/s3DropBucket.ts
@@ -672,7 +672,6 @@ function selectiveLogging(level:string, index: string,  msg:string) {
   
   const selDeb = process.env.S3DropBucketSelectiveDebug ?? S3DBConfig.SelectiveDebug ?? "_103,_104,_511,"
     
-  debugger
   const li = `_${index},`
   if ( Number(index) < 100 || selDeb.indexOf(li) > -1 || index === dVerb )
   {
